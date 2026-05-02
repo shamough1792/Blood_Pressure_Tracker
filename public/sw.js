14cb4b69e3b40bdc401fe3aebc74d44688cd0d5f
shamough1792 <kinwanglam@gmail.com>
2026-05-03 00:53:24 +0800

    fix: add PNG icons and install button for PWA compatibility

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,9 @@
-const CACHE_NAME = 'bp-tracker-v1';
+const CACHE_NAME = 'bp-tracker-v2';
 const STATIC_ASSETS = [
   '/styles.css',
   '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
   '/icons/icon-192.svg',
   '/icons/icon-512.svg',
   '/favicon.ico'
